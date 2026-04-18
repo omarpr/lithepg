ef59a42937414da82b3f9b3a8a0566292248fd2f
Omar Soto-Fortuño <4412916+omarpr@users.noreply.github.com>
2026-04-18 05:26:22 -0400

    build: add PostgresNIO and macOS platform to Package.swift
    Signed-off-by: omarpr <omarpr@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,15 +1,24 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "lithepg",
+    platforms: [
+        .macOS(.v14),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.21.0"),
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "lithepg"
+            name: "lithepg",
+            dependencies: [
+                .product(name: "PostgresNIO", package: "postgres-nio"),
+            ]
+        ),
+        .testTarget(
+            name: "lithepgTests",
+            dependencies: ["lithepg"]
         ),
     ]
 )
