18c3eb3bd10929be2e1e4e87be0803793266b304
Omar Soto-Fortuño <4412916+omarpr@users.noreply.github.com>
2026-05-03 17:22:53 -0400

    feat(app): preserve results per query tab
    Signed-off-by: omarpr <omarpr89@users.noreply.github.com>

--- Sources/LithePGApp/QueryTab.swift
+++ Sources/LithePGApp/QueryTab.swift
@@ -1,13 +1,16 @@
 import Foundation
+import LithePGCore
 
 public struct QueryTab: Identifiable, Equatable {
     public let id: UUID
     public var title: String
     public var text: String
+    public var lastResult: QueryResult?
 
-    public init(id: UUID = UUID(), title: String, text: String = "") {
+    public init(id: UUID = UUID(), title: String, text: String = "", lastResult: QueryResult? = nil) {
         self.id = id
         self.title = title
         self.text = text
+        self.lastResult = lastResult
     }
 }
