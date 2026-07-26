4ed40a5177dda744084de39e063e0c17be0377b0
omarpr <4412916+omarpr@users.noreply.github.com>
2026-07-26 08:09:44 -0400

    chore(release): prepare v1.0.5-preview.1
    Signed-off-by: omarpr <4412916+omarpr@users.noreply.github.com>

--- packaging/homebrew/lithepg.rb
+++ packaging/homebrew/lithepg.rb
@@ -3,8 +3,8 @@
 # visible unsigned-build warning to the copy published in the external tap.
 
 cask "lithepg" do
-  version "1.0.4-preview.1"
-  sha256 "319e7f4ee9cb81d00cb40e10c7d9cb553c3972092a58dd1a39515c763a17c3e2"
+  version "1.0.5-preview.1"
+  sha256 "c58735a4f205f91caccffb3f12318e81440ee5ecc0d0fca15e81250bf716ae31"
 
   url "https://github.com/omarpr/lithepg/releases/download/v#{version}/LithePG-#{version}.zip",
       verified: "github.com/omarpr/lithepg/"
